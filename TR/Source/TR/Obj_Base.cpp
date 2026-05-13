1bea50139d034e25ec3be204254ec5d2806f0411
IMBOT-INC <master@i-mbot.info>
2026-05-13 15:00:10 +0900

    ObjBase 변경

--- TR/Source/TR/Obj_Base.cpp
+++ TR/Source/TR/Obj_Base.cpp
@@ -5,8 +5,8 @@
 
 AObj_Base::AObj_Base()
 {
-	PrimaryActorTick.bCanEverTick = false;
-
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = false;
 	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
 	RootComponent = CollisionBox;
 
@@ -18,6 +18,48 @@ AObj_Base::AObj_Base()
 			&AObj_Base::OnObstacleBeginOverlap
 	);
 }
+
+
+void AObj_Base::MoveObject(float DeltaTime)
+{
+	UE_LOG(LogTemp, Warning, TEXT("MoveObject"));
+
+	if (!IsMove)
+	{
+		return;
+	}
+
+	FVector CurrentLocation = GetActorLocation();
+	FVector NextLocation = CurrentLocation + (TargetVelocity * DeltaTime);
+
+	float MovedDistance = FVector::Dist(StartLocation, NextLocation);
+
+	if (MovedDistance >= TargetDistance)
+	{
+		// 오버슛 방지: 목표 위치로 보정
+		FVector Direction = TargetVelocity.GetSafeNormal();
+		NextLocation = StartLocation + Direction * TargetDistance;
+
+		SetActorLocation(NextLocation);
+
+		if (IsLoop)
+		{
+			// 현재 목표 지점을 새로운 시작점으로 잡고 반대 방향으로 이동
+			StartLocation = NextLocation;
+			TargetVelocity = -TargetVelocity;
+		}
+		else
+		{
+			// IsLoop 없다면  Tick 중지
+			SetActorTickEnabled(false);
+		}
+
+		return;
+	}
+
+	SetActorLocation(NextLocation);
+}
+
 void AObj_Base::OnObstacleBeginOverlap(
 	  UPrimitiveComponent* OverlappedComponent,
 	  AActor* OtherActor,
@@ -54,4 +96,18 @@ void AObj_Base::OnObstacleBeginOverlap(
 			EffectType
 	);
 }
+}
+
+void AObj_Base::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	MoveObject(DeltaTime);
+}
+
+void AObj_Base::BeginPlay()
+{
+	Super::BeginPlay();
+	StartLocation = GetActorLocation();
+	SetActorTickEnabled(IsMove);
+		
 }
